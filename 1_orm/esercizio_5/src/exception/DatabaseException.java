0c714dfc2153ea10d30574f74e1648ee88fe0013
Canalella <mario.canalella-cic [at] ibm [dot] com>
2022-06-27 15:32:55 +0200

    upload esercizio 5

---
+++ 1_orm/esercizio_5/src/exception/DatabaseException.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(final Throwable cause) {
+        super(cause);
+    }
+}
