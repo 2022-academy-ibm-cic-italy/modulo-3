6081273c820dd14db602c1df1c2d09c53b4c9ced
Canalella <mario.canalella-cic [at] ibm [dot] com>
2022-06-30 17:03:16 +0200

    upload esercizio_2 junit

---
+++ 4_junit/esercizio_2/src/main/java/Database.java
@@ -0,0 +1,14 @@
+public class Database {
+
+    public boolean isAvailable() {
+        return false;
+    }
+
+    public int getUniqueId() {
+        return 42;
+    }
+
+    public void setUniqueId(int value) {
+        return;
+    }
+}
