b3fc51206a730e1bd2c4276b3545a3e60f772fa7
Canalella <mario.canalella-cic [at] ibm [dot] com>
2022-06-30 17:10:29 +0200

    upload demo mokito con jdbc

---
+++ 4_junit/demo_mokito_jdbc/src/main/java/com/esempio/test_mockito_jdbc/DataAccessException.java
@@ -0,0 +1,23 @@
+package com.esempio.test_mockito_jdbc;
+
+public class DataAccessException extends RuntimeException {
+
+	private static final long serialVersionUID = -4056262020632206194L;
+
+	public DataAccessException() {
+		super();
+	}
+
+	public DataAccessException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DataAccessException(String message) {
+		super(message);
+	}
+
+	public DataAccessException(Throwable cause) {
+		super(cause);
+	}
+
+}
